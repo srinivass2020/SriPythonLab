f84e54a547efa45096d54eb9fe61d06b17836578
Srinivas Chilukuri <srinivass2020@gmail.com>
2025-11-02 21:56:18 -0500

    AppliedDataScienceWithPython_V1

---
+++ Simplilearn/MyPractice/AppliedDataScienceWithPython/Live Classes/LabWork/NumPyIntro.py
@@ -0,0 +1,7 @@
+import numpy as np
+num_array=[20,30,40,50,60]
+array=np.array(num_array)
+print("Array Elements:", array)
+print("Array Shape:", array.shape)
+print("Array Data Type:", array.dtype)
+print("Array Size:", array.size)
